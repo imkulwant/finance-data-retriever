e5badc59fed2b2407b7d92e9a54bb8543692f110
singh.kulwant@gmx.com <singh.kulwant@gmx.com>
2019-10-31 15:19:00 +0530

    31 Oct added spring boot project

---
+++ src/main/java/com/rfa/RssFeedsAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.rfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssFeedsAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssFeedsAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rfa/RssFeedsAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssFeedsAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
