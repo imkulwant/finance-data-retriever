9401eff95867e659a79888c5df9c73e2232f5796
singh.kulwant@gmx.com <singh.kulwant@gmx.com>
2019-11-01 23:15:01 +0530

    1 Nov jdbc authentication with h2

--- src/main/java/com/rfa/controller/HomeResource.java
+++ src/main/java/com/rfa/controller/HomeResource.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class RFAController {
+public class HomeResource {
 
 	@GetMapping(value = "/")
 	public String defaultHome() {
--- src/main/java/com/rfa/security/SecurityConfiguration.java
+++ src/main/java/com/rfa/security/SecurityConfiguration.java
@@ -1,22 +1,37 @@
 package com.rfa.security;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	DataSource dataSource;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN")
-			.and().withUser("user").password("user").roles("USER");
+		/*
+		 * in memory authentication
+		 * 
+		 * auth.inMemoryAuthentication().withUser("admin").password("admin").roles(
+		 * "ADMIN") .and().withUser("user").password("user").roles("USER");
+		 */
 
+		auth.jdbcAuthentication().dataSource(dataSource).withDefaultSchema()
+				.withUser(User.builder().username("user").password("user").roles("USER").build())
+				.withUser(User.builder().username("admin").password("admin").roles("ADMIN").build());
 	}
 
 	@Override
