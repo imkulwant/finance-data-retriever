9f137e3627bc99ffe4db1e18a959ac63df27388c
singh-kulwant <singh.kulwant@gmx.com>
2020-10-11 12:51:50 +0200

    10-11: converted to maven

--- src/main/java/com/sds/app/StockDataServiceApplication.java
+++ src/main/java/com/sds/app/StockDataServiceApplication.java
@@ -1,13 +1,13 @@
-package com.rfa;
+package com.sds.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RssFeedsAggregatorApplication {
+public class StockDataServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RssFeedsAggregatorApplication.class, args);
+		SpringApplication.run(StockDataServiceApplication.class, args);
 	}
 
 }
